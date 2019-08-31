a49619f90510d48c055acfc713e3fe4c393c167a
jmjuanico <joey.juanico@gmail.com>
2019-08-31 09:30:58 +0800

    auto saving updates

--- savedotfiles.sh
+++ savedotfiles.sh
@@ -7,7 +7,8 @@
 ########## Variables
 
 dir=~/dotfiles
-filestosave="bashrc vimrc zshrc oh-my-zsh gitignore"
+githooksdir=~/.git-templates/hooks
+filestosave="bashrc vimrc zshrc oh-my-zsh gitignore gitconfig"
 
 ##########
 
@@ -22,6 +23,8 @@ echo "Go to $dir and save dotfiles"
 for file in $filestosave; do
     cp ~/.$file $dir/$file
 done
+
+cp ~/.git-templates/hooks dir/git-hooks
 
 cd $dir
 git add .
