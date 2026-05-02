331fbd7d413907daf42d98eecab5ca7fb766df27
jmjuanico <joey.juanico@gmail.com>
2026-05-02 12:01:54 +0800

    auto saving updates

--- savedotfiles.sh
+++ savedotfiles.sh
@@ -4,12 +4,24 @@
 # Saves dotfiles backup to git
 ############################
 
+########## Variables
+
 dir=~/dotfiles
-githooks=~/.git-templates/hooks
+filestosave="
+bashrc
+vimrc
+zshrc
+gitignore
+gitconfig
+emacs.d/init.el
+emacs.d/boot.org
+vim/coc-settings.json
+.git-templates/hooks
+"
 
-filestosave="bashrc vimrc zshrc gitignore gitconfig emacs.d/init.el emacs.d/boot.org vim/coc-settings.json"
+##########
 
-echo "Creating $dir"
+echo "Creating $dir for backup of dotfiles..."
 mkdir -p "$dir"
 
 echo "Backing up dotfiles..."
@@ -18,24 +30,26 @@ for file in $filestosave; do
     src="$HOME/.$file"
     dest="$dir/$file"
 
-    if [ -f "$src" ]; then
+    if [ -e "$src" ]; then
         mkdir -p "$(dirname "$dest")"
-        cp "$src" "$dest"
+        cp -r "$src" "$dest"
         echo "Saved $file"
     else
         echo "Skipping missing: $file"
     fi
 done
 
-echo "Saving git hooks..."
-cp -r "$githooks" "$dir/git-hooks"
+echo "Entering dotfiles repo..."
+cd "$dir" || exit 1
 
-cd "$dir" || exit
-
+echo "Staging changes..."
 git add .
 
+# Prevent empty commits
 git diff --cached --quiet && echo "No changes to commit" && exit 0
 
+echo "Committing changes..."
 git commit -m "auto saving updates"
 
+echo "Pushing to remote..."
 git push origin HEAD
